d4b789cda70fcfbe4ebaa2e94d02afc85e01abfa
Pavlen <pavlen@MacBook-Air-Rina.local>
2023-12-24 19:12:51 +0300

    Create title_image_json & title_link_json

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as BS
 import os
 import json
 import csv
+# from fake_useragent import Useragent
 
 
 
@@ -27,20 +28,61 @@ with open("index.html") as file:
     src = file.read()
 
 soup = BS(src, "lxml")
-# В качестве параметра поиска передадим класс ссылок(id)
-all_articles_hrefs = soup.find_all('div', id='sticky-content', limit=10)
-# all_articles_hrefs = soup.find_all("a", class_="b-news-feed-home g-link-block d-flex flex-row mb-1 py-1")
+# # В качестве параметра поиска передадим класс ссылок(id)
+# all_articles_hrefs = soup.find_all('div', id='sticky-content')
 
-#Создадим переменную {} и далее будем его наполнять
-all_articles_dict = {}
+# Создаем пустой словарь для хранения заголовка и фото к сатье
+titles_images_dict = {}
+# Находим все элементы с классом "b-news-feed-home"
+news_elements = soup.find_all('div', class_="b-news-feed-home")
 
-for item in all_articles_hrefs:
-    # item_text = item.find("div", class_="b-news-feed-home__title g-color--darkgrey font-weight-bold py-1").text.strip()
-    item_text = item.find("a")["title"]
-    item_href = item.find("a")["href"]
-    print(f"{item_text}: {item_href}")
-    all_articles_dict[item_text] = item_href
+# Проходимся по каждому элементу и извлекаем нужную информацию
+for element in news_elements:
+    # Находим заголовок статьи
+    title = element.find(class_="b-news-feed-home__title").text.strip()
+    
+    # # Находим ссылку на статью
+    # link_element = element.find("a", href=True)
+    # link = link_element["href"] if link_element else ""
+
+    # Находим ссылку на картинку
+    image_element = element.find("img", src=True)
+    image = image_element["data-src"] if image_element else ""
+
+    # Добавляем статью в словарь
+    # articles_dict[title] = {"link": link, "image": image}
+    titles_images_dict[title] = {"image": image}
+
+# Сохраняем словарь со статьями в формате JSON
+with open("json_data/title_image.json", "w", encoding="utf-8") as file:
+    json.dump(titles_images_dict, file, indent=4,  ensure_ascii=False)
+
+# Выводим сообщение об успешном завершении
+print("Заголовки и Картинки к статьям успешно спарсены и сохранены в файл articles.json")
+
+
+#Создадим переменную {} и далее будем его наполнять заголовком и ссылкой к ней
+# title_link_dict = []
+title_link_dict = {}
+
+# Находим ссылку и заголовок статьи
+# <div id="sticky-content">
+links = soup.find_all("a", href=True, title=True)
+# href = links["href"]
+# title = links["title"]
+
+# Заполняем список словарей
+for link in links:
+    href = link["href"]
+    title = link["title"]
+    title_link_dict[title] = href
+
 
 # Открываем файл на запись JSON
-with open("all_articles_dict.json", "w", encoding='utf-8') as file:
-    json.dump(all_articles_dict, file, indent=4, ensure_ascii=False)
+with open("json_data/title_link.json", "w", encoding='utf-8') as file:
+    json.dump(title_link_dict, file, indent=4, ensure_ascii=False)
+
+
+
+# if __name__ == "__main__":
+#     result = main()
